ec019987ee4868a80e8721160e44d230d5ff177f
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-03-17 14:16:48 +0100

    Add timings to run_study.py

--- experiments/epf_germany/00_run_study.py
+++ experiments/epf_germany/00_run_study.py
@@ -1,18 +1,18 @@
-import subprocess
 import os
+import subprocess
 
 # Make the folders
-try: 
+try:
     os.makedirs("/figues", exist_ok=False)
 except OSError:
     print("/figures folder exists")
 
-try: 
+try:
     os.makedirs("/results", exist_ok=False)
 except OSError:
     print("/results folder exists")
 
-try: 
+try:
     os.makedirs("/tables", exist_ok=False)
 except OSError:
     print("/tables folder exists")
@@ -25,5 +25,6 @@ subprocess.run(["python", "04_multivariate_online_distreg.py"])
 subprocess.run(["python", "05_calculate_scores.py"])
 subprocess.run(["python", "06_make_figures.py"])
 subprocess.run(["python", "07_make_tables_error_scores.py"])
+subprocess.run(["python", "08_make_table_timings.py"])
 
 print("Successfully finished simulation and created all reproduction files.")
